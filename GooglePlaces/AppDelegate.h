32c9c9ad90c9caaa4b440d3d0728fab27985444a
jvanlint <jvanlint@mac.com>
2012-05-01 09:20:47 +0200

    Initial Commit

---
+++ GooglePlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GooglePlaces
+//
+//  Created by van Lint Jason on 1/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GooglePlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GooglePlaces
+//
+//  Created by van Lint Jason on 1/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
